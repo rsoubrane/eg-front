a03ceca92790d95b71051bc6304c9fd730236c3d
Romain <romain@Romains-MacBook-Pro.local>
2019-09-08 21:37:08 +0200

    Init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+
+import Navbar from "./components/Navbar/Navbar";
+import ExercisesList from "./components/Exercises/ExercisesList";
+import EditExercise from "./components/Exercises/EditExercise";
+import CreateExercise from "./components/Exercises/CreateExercise";
+import CreateUser from "./components/User/CreateUser";
 
 function App() {
 	return (
-		<div>
-			<h1>Hello World !</h1>
-		</div>
+		<Router>
+			<Navbar />
+			<div className='container'>
+				<br />
+				<Route path='/' exact component={ExercisesList} />
+				<Route path='/edit/:id' component={EditExercise} />
+				<Route path='/create' component={CreateExercise} />
+				<Route path='/user' component={CreateUser} />
+			</div>
+		</Router>
 	);
 }
 
